bb4d8b6337f6bd709a2c0cd5ce7965773423948c
Ashish Pundhir <81098495+ashishpundhir@users.noreply.github.com>
2023-01-11 09:11:38 +0530

    Add files via upload

---
+++ analog.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+   d = new Date();
+   htime = d.getHours();
+   mtime = d.getMinutes();
+   stime = d.getSeconds();
+   hrotation = 30*htime + mtime/2 + stime/10;
+   mrotation = 6*mtime + stime/10;
+   srotation = 6*stime;
+
+   hour.style.transform = 'rotate(${hrotation}deg)';
+   minute.style.transform = 'rotate(${mrotation}deg)';  
+   seconds.style.transform = 'rotate(${srotation}deg)';
+}, 1000);
